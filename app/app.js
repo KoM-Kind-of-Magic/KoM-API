8cad6c7239cb9ee5ee456241e4e01ba1012034f5
plnicolas-epitech <pierre-louis.nicolas@epitech.eu>
2022-07-21 13:51:44 +0200

    API : setup cards simple route

--- app/app.js
+++ app/app.js
@@ -1,27 +1,28 @@
 require('dotenv').config();
 const express = require('express')
 const app = express()
-const port = 8080;
-
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize(process.env.MYSQL_DATABASE , process.env.MYSQL_ROOT_USERNAME, process.env.MYSQL_ROOT_PASSWORD, {
-  host: 'mysql',
-  dialect: 'mysql'
-});
-
-try {
-  sequelize.authenticate();
-} catch (error) {
-  console.error('Unable to connect to the database:', error);
-}
+const port = 8089;
 
 const database = require('./database/database')
 
 app.get('/cards', (req, res) => {
-  result = database.searchCards()
-  console.log(typeof(result));
-  res.json(result)
+  database.getCards()
+  .then((data) => {
+    res.json({'status': 'success', 'data': data})
+  })
+  .catch((error) => {
+    res.json({'status': 'failure', 'error': error})
+  })
+})
+
+app.get('/cards/search', (req, res) => {
+  database.searchCards()
+  .then((data) => {
+    res.json({'status': 'success', 'data': data})
+  })
+  .catch((error) => {
+    res.json({'status': 'failure', 'error': error})
+  })
 })
 
 app.get('/', (req, res) => {
