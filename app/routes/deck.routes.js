e7768b8e01332ae36530fca81d4761e119028301
plnicolas-epitech <pierre-louis.nicolas@epitech.eu>
2022-09-09 12:04:53 +0200

    FIX : cleaning and standardization of deck functions

--- app/routes/deck.routes.js
+++ app/routes/deck.routes.js
@@ -3,8 +3,8 @@ const router = express.Router();
 
 const deckController = require("../controller/deck.controller");
 
-router.get("/", deckController.deck);
-router.get("/fields", deckController.fields);
+router.get("/", deckController.deck); // for the moment this route only retrieves the first 20 decks from the database
+// router.get("/fields", deckController.fields); //I don't think this route will be used so i'll leave it here commented
 
 // POST	Create
 router.post("/", deckController.create);
@@ -13,7 +13,7 @@ router.get("/:id", deckController.deck_by_id);
 // PUT	Update/Replace
 router.put("/:id", deckController.update);
 // PATCH	Update/Modify
-// router.patch("/:id", deckController.patch);
+router.patch("/:id", deckController.patch);
 // DELETE	Delete
 router.delete("/:id", deckController.delete);
 
