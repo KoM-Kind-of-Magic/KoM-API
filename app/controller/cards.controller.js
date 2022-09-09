b5b15f4ff7b6e9bbc97ac580f530b711678cc026
plnicolas-epitech <pierre-louis.nicolas@epitech.eu>
2022-09-09 12:32:41 +0200

    NEW : adding cards by uuid route

--- app/controller/cards.controller.js
+++ app/controller/cards.controller.js
@@ -6,3 +6,37 @@ exports.cards = async (req, res) => {
       .then((data) => {res.json({'status': 200, 'data': data});})
       .catch((error) => {res.json({'status': 500, 'error': error})})
 };
+
+exports.card_by_uuid = async (req, res) => {
+  try {
+    const uuid = req.params.uuid
+    Deck
+    .findOne({
+      where: {
+        uuid: uuid
+      }
+    })
+    .then((data) => {
+      if(data !== null) {
+        return res.status(200).send({
+          message: "Card stored in data key",
+          data: data
+        });
+      }
+      else {
+        return res.status(404).send({
+          message: "Card not found",
+        });
+      }
+    })
+    .catch((error) => {
+      return res.status(500).send({
+        message: error.message,
+      });
+    })
+  } catch (error) {
+    return res.status(500).send({
+      message: error.message,
+    });
+  }
+};
--- app/routes/cards.routes.js
+++ app/routes/cards.routes.js
@@ -3,6 +3,12 @@ const router = express.Router();
 
 const cardsController = require("../controller/cards.controller");
 
-router.get("/", cardsController.cards);
+router.get("/", cardsController.cards);// for the moment this route only retrieves the first 20 cards from the database
+
+// GET	Read
+router.get("/:uuid", cardsController.card_by_uuid);
+
+// GET	Search
+// router.get("/search", cardsController.search);
 
 module.exports = router;
