f5b52831abec59c513af6ba68d9fa4fc71562ff1
plnicolas-epitech <pierre-louis.nicolas@epitech.eu>
2022-11-17 11:01:53 +0100

    FIX cards search route like wrong typo

--- app/routes/cards.routes.js
+++ app/routes/cards.routes.js
@@ -5,10 +5,10 @@ const cardsController = require("../controller/cards.controller");
 
 router.get("/", cardsController.cards);// for the moment this route only retrieves the first 20 cards from the database
 
-// GET	Read
-router.get("/:uuid", cardsController.card_by_uuid);
-
 // GET	Search
 router.get("/search", cardsController.search);
+
+// GET	Read
+router.get("/:uuid", cardsController.card_by_uuid);
 
 module.exports = router;
