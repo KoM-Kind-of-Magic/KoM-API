e05b051135a46e20642197ab58eefd4ddfb3d78a
Mikail <8000henri@gmail.com>
2023-03-09 15:44:19 +0100

    chore:  added param to use docker or not

--- app/conf/database.js
+++ app/conf/database.js
@@ -2,13 +2,14 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const { Sequelize } = require('sequelize');
+host_val = process.env.USE_DOCKER ? 'mariadb' : '127.0.0.1';
 
 const sequelize = new Sequelize(
   process.env.MYSQLDB_DATABASE,
   process.env.MYSQLDB_USER,
   process.env.MYSQLDB_PASSWORD, 
   {
-    host: '127.0.0.1',
+    host: host_val,
     dialect: 'mariadb',
     define: {
       timestamps: false 
