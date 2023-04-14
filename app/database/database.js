e5bc9b9b377068900674dc5db6b7ab5e5d46e242
jenoh <30670005+jenoh@users.noreply.github.com>
2023-04-14 12:11:08 +0200

    Maj ansible master (#74)
    * Maj ansible branch to pull
    
    * Add jwt
    
    * Fix env var
    
    * Maj var
    
    * Maj root
    
    * maj pm2
    
    * fix user

--- app/database/database.js
+++ app/database/database.js
@@ -6,7 +6,7 @@ console.log(process.env.MYSQL_DATABASE);
 
 const sequelize = new Sequelize(
   process.env.MYSQL_DATABASE,
-  process.env.MYSQL_USERNAME,
+  process.env.MYSQL_USER,
   process.env.MYSQL_PASSWORD, 
   {
     host: 'localhost',
