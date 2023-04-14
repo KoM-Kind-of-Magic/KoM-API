a63aaf1244412b467e86be7dc365c9de3c6c4b84
jenoh <30670005+jenoh@users.noreply.github.com>
2023-04-14 16:43:39 +0200

    Maj ansible master (#76)
    * Maj ansible branch to pull
    
    * Add jwt
    
    * Fix env var
    
    * Maj var
    
    * Maj root
    
    * maj pm2
    
    * fix user
    
    * Modif
    
    * fix

--- app/conf/database.js
+++ app/conf/database.js
@@ -2,14 +2,14 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const { Sequelize } = require('sequelize');
-// host_val = process.env.USE_DOCKER ? 'mariadb' : '127.0.0.1';
-
+host_val = process.env.USE_DOCKER ? 'mariadb' : '127.0.0.1';
+console.log(process.env.MYSQL_DATABASE,process.env.MYSQL_USER, process.env.MYSQL_PASSWORD + "lfdsjfldsj")
 const sequelize = new Sequelize(
   process.env.MYSQL_DATABASE,
   process.env.MYSQL_USER,
   process.env.MYSQL_PASSWORD, 
   {
-    host: "127.0.0.1",
+    host: host_val,
     dialect: 'mariadb',
     define: {
       timestamps: false 
