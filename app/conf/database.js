35d95634c991524c88614321c6041f3bb0966b13
jenoh <memint.contact@gmail.com>
2023-04-14 15:46:03 +0200

    Modif

--- app/conf/database.js
+++ app/conf/database.js
@@ -2,21 +2,19 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const { Sequelize } = require('sequelize');
-host_val = process.env.USE_DOCKER ? 'mariadb' : '127.0.0.1';
+// host_val = process.env.USE_DOCKER ? 'mariadb' : '127.0.0.1';
 
 const sequelize = new Sequelize(
-  process.env.MYSQLDB_DATABASE,
-  process.env.MYSQLDB_USER,
-  process.env.MYSQLDB_PASSWORD, 
+  process.env.MYSQL_DATABASE,
+  process.env.MYSQL_USER,
+  process.env.MYSQL_PASSWORD, 
   {
-    host: host_val,
+    host: "127.0.0.1",
     dialect: 'mariadb',
     define: {
       timestamps: false 
     }
   }
 );
-
-
 
 module.exports = sequelize
